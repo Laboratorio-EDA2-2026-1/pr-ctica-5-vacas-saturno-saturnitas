caf378d4e8fb2b66c84994a4d77824c0976717b2
victor_ostria <victorostriamacario@gmail.com>
2025-09-26 07:46:18 -0600

    Update counting_sort.c
    Add EJERCICIO 1

--- counting_sort.c
+++ counting_sort.c
@@ -1,18 +1,19 @@
 // Escribe aquí la implementación de los ejercicios de Counting Sort.
 
+//EJERCICIO 1
 #include <stdio.h>
 #include <string.h>
-
+    
 void countingSortString(char s[]) {
     int count[26] = {0}; // arreglo de conteo
     int n = strlen(s);
-
-    // Paso 1: contar ocurrencias
+    
+    //Contar cuantas veces aparece una letra
     for (int i = 0; i < n; i++) {
         count[s[i] - 'a']++;
     }
-
-    // Paso 2: reconstruir cadena ordenada
+        
+    //Ordenar arreglo
     int index = 0;
     for (int i = 0; i < 26; i++) {
         while (count[i] > 0) {
@@ -22,16 +23,26 @@ void countingSortString(char s[]) {
     }
     s[index] = '\0'; // terminar cadena
 }
-
+    
 int main() {
-    char s1[] = "edsab";
-    char s2[] = "geeksforgeeks";
+    char s1[100];
+    char s2[100];
+
+    printf("Ingresar todas las cadenas en minúsculas...\n");
+    printf("Ingrese cadena 1: ");
+    scanf("%s", s1); //edsab
+
+    printf("Ingrese cadena 2: ");
+    scanf("%s", s2); //geekforgeeks
+
+    printf("\nLetras en orden lexicográfico...\n");
 
     countingSortString(s1);
     countingSortString(s2);
 
-    printf("%s\n", s1); // abdes
-    printf("%s\n", s2); // eeeefggkkorss
+    printf("%s\n", s1); //abdes
+    printf("%s\n", s2); //eeeefggkkorss
 
     return 0;
 }
+
