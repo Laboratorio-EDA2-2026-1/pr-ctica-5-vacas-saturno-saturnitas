51cc4571ec5fa71329cb15bb0c47f64bf05c1c63
victor_ostria <victorostriamacario@gmail.com>
2025-09-26 08:56:52 -0600

    Update busqueda_lineal.c

--- busqueda_lineal.c
+++ busqueda_lineal.c
@@ -11,22 +11,6 @@ int linearSearch(int arr[], int n, int key) {
     return -1; // no encontrado
 }
 
-int linearSearchImproved(int arr[], int n, int key) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == key) {
-            // Transposición: mover elemento hacia la posición anterior
-            if (i > 0) {
-                int temp = arr[i];
-                arr[i] = arr[i - 1];
-                arr[i - 1] = temp;
-                return i - 1; // ahora está una posición antes
-            }
-            return i;
-        }
-    }
-    return -1;
-}
-
 int linearSearchRecursive(int arr[], int n, int key, int index) {
     if (index >= n) 
         return -1; // no encontrado
@@ -51,6 +35,22 @@ int linearSearchSentinel(int arr[], int n, int key) {
     return -1;    // no encontrado
 }
 
+int linearSearchImproved(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            // Transposición: mover elemento hacia la posición anterior
+            if (i > 0) {
+                int temp = arr[i];
+                arr[i] = arr[i - 1];
+                arr[i - 1] = temp;
+                return i - 1; // ahora está una posición antes
+            }
+            return i;
+        }
+    }
+    return -1;
+} //Al último para que la transposición no afecte el índice de los demás
+
 int main() {
     int arr[] = {10, 23, 45, 70, 11, 15};
     int n = sizeof(arr) / sizeof(arr[0]); //tamaño del arreglo
@@ -64,9 +64,9 @@ int main() {
     printf("\nKey: %d", key);
 
     printf("\nBúsqueda lineal simple: índice %d\n", linearSearch(arr, n, key));
-    printf("Búsqueda lineal mejorada: índice %d\n", linearSearchImproved(arr, n, key));
     printf("Búsqueda lineal recursiva: índice %d\n", linearSearchRecursive(arr, n, key, 0));
     printf("Búsqueda lineal con centinela: índice %d\n", linearSearchSentinel(arr, n, key));
+    printf("Búsqueda lineal mejorada: índice %d\n", linearSearchImproved(arr, n, key));
 
     return 0;
 }
